593ed98e67865f2a6f294481b486941dfc9076c8
Egidio De-Souza <egidio.de-souza@hotmail.co.uk>
2021-07-08 16:04:51 +0100

    Added validations to the models

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,9 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id,
+  message: "This is already in the list" }
+
 end
